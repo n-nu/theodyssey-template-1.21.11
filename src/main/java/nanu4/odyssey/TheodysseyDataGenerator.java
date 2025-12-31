0e5db8d6dac9e85bf1bff0e795b02ebbe1ab0faa
Emil Estrada <eestrada4@crimson.ua.edu>
2025-12-31 11:34:14 -0600

    first commit

---
+++ src/main/java/nanu4/odyssey/TheodysseyDataGenerator.java
@@ -0,0 +1,11 @@
+package nanu4.odyssey;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheodysseyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/nanu4/odyssey/theodysseyClient.java
@@ -0,0 +1,10 @@
+package nanu4.odyssey;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class theodysseyClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
